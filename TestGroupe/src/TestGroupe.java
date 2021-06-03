ce6ff405175278fb672f7c16c550166bd8476947
Tara LEMAIRE <tara.lemaire@outlook.fr>
2021-06-03 15:48:37 +0200

    Version 2 - Tara

--- TestGroupe/src/TestGroupe.java
+++ TestGroupe/src/TestGroupe.java
@@ -4,7 +4,7 @@ public class TestGroupe {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Projet en groupe version 1 - Dorian");
-
+		System.out.println("Projet en groupe version 2 - Tara");
 	}
 
 }
