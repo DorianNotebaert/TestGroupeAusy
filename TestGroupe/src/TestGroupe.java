96926e4e3873c169d5757a33b867e1b8f3ee8f4c
Dorian NOTEBAERT <dorian.not@gmail.com>
2021-06-03 15:45:39 +0200

    Premier Commit Dorian

--- TestGroupe/src/TestGroupe.java
+++ TestGroupe/src/TestGroupe.java
@@ -1,6 +1,10 @@
 
 public class TestGroupe {
+
 	public static void main(String[] args) {
+		// TODO Auto-generated method stub
 		System.out.println("Projet en groupe version 1 - Dorian");
+
 	}
+
 }
